ef99776ae199f2d6433be00d35fd9fdcaf84226f
CJ Pais <cj@cjpais.com>
2026-01-19 10:20:57 +0800

    add experimental features to ui + reorg everything

--- src/components/settings/advanced/AdvancedSettings.tsx
+++ src/components/settings/advanced/AdvancedSettings.tsx
@@ -10,27 +10,58 @@ import { AutostartToggle } from "../AutostartToggle";
 import { PasteMethodSetting } from "../PasteMethod";
 import { ClipboardHandlingSetting } from "../ClipboardHandling";
 import { useModelStore } from "../../../stores/modelStore";
+import { PostProcessingToggle } from "../PostProcessingToggle";
+import { AppendTrailingSpace } from "../AppendTrailingSpace";
+import { HistoryLimit } from "../HistoryLimit";
+import { RecordingRetentionPeriodSelector } from "../RecordingRetentionPeriod";
+import { ExperimentalToggle } from "../ExperimentalToggle";
+import { useSettings } from "../../../hooks/useSettings";
 
 export const AdvancedSettings: React.FC = () => {
   const { t } = useTranslation();
   const { currentModel, getModelInfo } = useModelStore();
+  const { getSetting } = useSettings();
   const currentModelInfo = getModelInfo(currentModel);
   const showTranslateToEnglish =
     currentModelInfo?.engine_type === "Whisper" && currentModel !== "turbo";
+  const experimentalEnabled = getSetting("experimental_enabled") || false;
+
   return (
     <div className="max-w-3xl w-full mx-auto space-y-6">
-      <SettingsGroup title={t("settings.advanced.title")}>
+      <SettingsGroup title={t("settings.advanced.groups.app")}>
         <StartHidden descriptionMode="tooltip" grouped={true} />
         <AutostartToggle descriptionMode="tooltip" grouped={true} />
         <ShowOverlay descriptionMode="tooltip" grouped={true} />
+        <ModelUnloadTimeoutSetting descriptionMode="tooltip" grouped={true} />
+        <ExperimentalToggle descriptionMode="tooltip" grouped={true} />
+      </SettingsGroup>
+
+      <SettingsGroup title={t("settings.advanced.groups.output")}>
         <PasteMethodSetting descriptionMode="tooltip" grouped={true} />
         <ClipboardHandlingSetting descriptionMode="tooltip" grouped={true} />
+      </SettingsGroup>
+
+      <SettingsGroup title={t("settings.advanced.groups.transcription")}>
         {showTranslateToEnglish && (
           <TranslateToEnglish descriptionMode="tooltip" grouped={true} />
         )}
-        <ModelUnloadTimeoutSetting descriptionMode="tooltip" grouped={true} />
         <CustomWords descriptionMode="tooltip" grouped />
+        <AppendTrailingSpace descriptionMode="tooltip" grouped={true} />
       </SettingsGroup>
+
+      <SettingsGroup title={t("settings.advanced.groups.history")}>
+        <HistoryLimit descriptionMode="tooltip" grouped={true} />
+        <RecordingRetentionPeriodSelector
+          descriptionMode="tooltip"
+          grouped={true}
+        />
+      </SettingsGroup>
+
+      {experimentalEnabled && (
+        <SettingsGroup title={t("settings.advanced.groups.experimental")}>
+          <PostProcessingToggle descriptionMode="tooltip" grouped={true} />
+        </SettingsGroup>
+      )}
     </div>
   );
 };
